8ae651aecb9e8556502ac4e9d5ba44462501c6f4
Ross Harding <rnh8wb@virginia.edu>
2018-04-03 10:10:15 -0400

    iOS app created

---
+++ PocketHinman/PocketHinman/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PocketHinman
+//
+//  Created by Ross Harding on 4/3/18.
+//  Copyright © 2018 Harding LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
